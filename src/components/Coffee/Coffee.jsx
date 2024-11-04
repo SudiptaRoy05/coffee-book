a17f501913488e55b3a6670ac4bcaa78d33875ea
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-05 00:53:43 +0600

    sorted

--- src/components/Coffee/Coffee.jsx
+++ src/components/Coffee/Coffee.jsx
@@ -1,26 +1,26 @@
 
-export default function Coffee({coffee}) {
-    const { name, image, category, origin, type, rating, popularity } =
-    coffee || {}; 
-    return (
-        <div className="card bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src={image}
-            alt={name}
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{name}</h2>
-          <p>{category}</p>
-          <p>{origin}</p>
-          <p>{type}</p>
-          <p>{rating}</p>
-          <p>{popularity}</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-  )
-}
+// export default function Coffee({coffee}) {
+//     const { name, image, category, origin, type, rating, popularity } =
+//     coffee || {}; 
+//     return (
+//         <div className="card bg-base-100 shadow-xl">
+//         <figure>
+//           <img
+//             src={image}
+//             alt={name}
+//           />
+//         </figure>
+//         <div className="card-body">
+//           <h2 className="card-title">{name}</h2>
+//           <p>{category}</p>
+//           <p>{origin}</p>
+//           <p>{type}</p>
+//           <p>{rating}</p>
+//           <p>{popularity}</p>
+//           <div className="card-actions justify-end">
+//             <button className="btn btn-primary">Buy Now</button>
+//           </div>
+//         </div>
+//       </div>
+//   )
+// }
